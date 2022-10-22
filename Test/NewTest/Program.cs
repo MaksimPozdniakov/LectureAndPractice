244379fc4b5ea300a5350ed101b95bb9fc3a8947
Maksim <p.m.pavlovich@ya.ru>
2022-10-22 20:01:42 +0300

    qqq

--- Test/NewTest/Program.cs
+++ Test/NewTest/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача №1. Напмшмте программу, которая будет изменять элементы массива с отриуательных на положительные.
+﻿// Задача №1. Напмшмте программу, которая будет изменять элементы массива с отрицательных на положительные.
 /*
 int[] NewArray(int size, int minValue, int maxValue)
 {
@@ -47,74 +47,149 @@ Console.Write("Мой новый массив = ");
 int[] result = ChengeArray(myArray);
 ShowArray(result);
 */
-// Задача №2. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-/*
-int Amount(int size)
-{
-    int count = 0;
 
-    for(int i = 0; i < size; i++)
+// Разобраться в методе решения
+
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+/*
+double[] IntersectionLines(double b1, double k1, double b2, double k2)
+{
+    double[] array = new double[2];
+    array[0] = (b2-b1)/(k1-k2);
+    array[1] = k1 * array[0] + b1;
+    return array;
+}
+void PrintIntersection(double[] array)
+{
+    Console.Write($"Точка пересечения = [{array[0]}; {array[1]}].");
+}
+
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+if(k1 == k2) Console.WriteLine("Прямые параллельны, точки пересечения нет.");
+else
+{
+    double[] result = IntersectionLines(b1,k1,b2,k2);
+    PrintIntersection(result);
+}
+Console.ReadLine();
+*/
+
+// Тоже разобраться
+
+/*
+int AmountPositiveNums(int amountNum)
+{
+    int amount = 0;
+    int count = 0;
+    do
     {
-        Console.Write($"Введите {i+1} число ");
-        int number = Convert.ToInt32(Console.ReadLine());
-            if(i > 0) count++;
-    }
+        Console.Write("Введите число: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+        if (num > 0) count++;
+        amount++;
+    } while (amount < amountNum);
     return count;
 }
 
-Console.Write("Сколько чисел будем проверять? ");
-int size = Convert.ToInt32(Console.ReadLine());
-int result = Amount(size); 
-Console.WriteLine($"Количество положительных чисел {result}");
+Console.Write("Введите желаемое количество чисел: ");
+int amountNum = Convert.ToInt32(Console.ReadLine());
+int result = AmountPositiveNums(amountNum);
+Console.WriteLine($"Количество положительных чисел = {result}");
+Console.ReadLine();
 */
 
-// теперь тоже самое только через массив.
 /*
-int[] CreateArray()
+void AmountPositiveNums()
 {
-    Console.Write("Сколько чисел будем проверять? ");
-    int size = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Минимальный эллемент массива ");
-    int min = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Максимальный эллемент массива ");
-    int max = Convert.ToInt32(Console.ReadLine());
-    
-    int[] myArray = new int[size];
+    Console.WriteLine("Введите число для проверки. Для завершения программы введите q.");
+    int count = 0;
+    int amount = 0;
+    while(true)
+    {
+        Console.Write("Введите число: ");
+        string? userInput = Console.ReadLine();
+        string stop = "q";
+        if(userInput != stop)
+        {
+            int num = Convert.ToInt32(userInput);
+            if (num > 0) count++;
+        }
+        else 
+        {
+        Console.WriteLine("Ввод окончен.");
+        Console.WriteLine($"Количество положительных чисел = {count}. Всего введено {amount} чисел.");
+        break;
+        }
+        amount++;
+    }
+}
 
+AmountPositiveNums();
+Console.ReadLine();
+*/
+
+// в этом решении обязательно разобраться 
+
+// Задача №4. Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний 
+// элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+/*
+Console.Clear();
+int[] CreateArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
     for (int i = 0; i < size; i++)
     {
-        myArray[i] = new Random().Next(min, max); 
+        array[i] = new Random().Next(minValue, maxValue + 1);
     }
-    return myArray;
+    return array;
 }
-
-void ShowArray(int[] myArray)
+void PrintArray(int[] array)
 {
-    for (int i = 0; i < myArray.Length; i++)
+    Console.Write("Created array: { ");
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(myArray[i] + " ");
+        Console.Write(array[i] + " ");
     }
-    Console.WriteLine();
+    Console.WriteLine("}.");
 }
-
-int PozitivNumbers(int[] myArray)
+/*
+int[] ProductElementsArray(int[] array)
 {
-    int count = 0;
-
-    for(int i = 0; i <= myArray.Length; i++)
+    int i = 0;
+    int size = array.Length;
+    int end = array.Length / 2;
+    if (size % 2 != 0)
     {
-        if(i > 0) count++;
+        int length = size / 2 + 1;
+        int[] newarray = new int[length];
+        while (i < end)
+        {
+            newarray[i] = array[i] * array[size - 1];
+            i++;
+            size--;
+        }
+        newarray[i] = array[end];
+        return newarray;
     }
-    return count;
+    else
+    {
+        int count = size / 2;
+        int[] newarray = new int[count];
+        while (i < end)
+        {
+            newarray[i] = array[i] * array[size - 1];
+            i++;
+            size--;
+        }
+        return newarray;
+    }
 }
-
-int[] myArray = CreateArray();
-ShowArray(myArray);
-
-Console.WriteLine();
-
-int result = PozitivNumbers(myArray);
-
-Console.Write($"Количество положительных чисел = {result}");
 */
-
